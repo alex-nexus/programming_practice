5e244d15a1f3a1d8b859f0076c5146ec8219ee71
Alex Chen <alkchen@gmail.com>
2013-08-28 01:23:20 -0700

    even shorter version

---
+++ power_set2.py
@@ -0,0 +1,11 @@
+def power_set(input_set):
+	if len(input_set)==0:
+		return [[]]	
+	n = input_set.pop()	
+	return reduce(lambda x,y:x+y, [[[n]+ps]+[ps] for ps in power_set(input_set)])
+	
+if __name__ == '__main__':
+	input_array = set(['A', 'B', 'C', 'D'])
+	print 'input_array', input_array	
+	output_array = power_set(input_array)
+	print 'output_array', output_array, len(output_array)
