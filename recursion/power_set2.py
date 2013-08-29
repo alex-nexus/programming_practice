43aa5b8e2e0548f63389518fcf71b41a876aabe5
Alex Chen <alkchen@gmail.com>
2013-08-28 22:13:09 -0700

    move

--- recursion/power_set2.py
+++ recursion/power_set2.py
@@ -5,7 +5,7 @@ def power_set(input_set):
 	return reduce(lambda x,y:x+y, [[[n]+ps]+[ps] for ps in power_set(input_set)])
 	
 if __name__ == '__main__':
-	input_array = set(['A', 'B', 'C', 'D'])
+	input_array = set(['A', 'B', 'C'])
 	print 'input_array', input_array	
 	output_array = power_set(input_array)
 	print 'output_array', output_array, len(output_array)
