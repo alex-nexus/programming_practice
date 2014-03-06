ebf8cbf7f2f58c2dc72be2652717972e2dc997f9
Alex Chen <alexchen@Alexs-MacBook-Air-2.local>
2014-03-05 22:27:09 -0800

    estimate pi

---
+++ estimate_pi.py
@@ -0,0 +1,11 @@
+from random import random
+
+def estimate_pi(n):
+	return 4.0*sum([in_circle() for i in xrange(n)]) / n
+	
+def in_circle():
+	x = random()
+	y = random()
+	return 1 if x*x + y*y < 1 else 0
+
+print estimate_pi(1000000)
